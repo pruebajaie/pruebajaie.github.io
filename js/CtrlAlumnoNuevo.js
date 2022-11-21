50ceb1d889403ed3986dc4c92a30b3b1cce94461
pruebajaie <pruebajaie@gmail.com>
2022-11-21 01:52:52 -0600

    cambio

--- js/CtrlAlumnoNuevo.js
+++ js/CtrlAlumnoNuevo.js
@@ -15,7 +15,7 @@ import {
 
 const daoAlumno =
   getFirestore().
-    collection("Alumno");
+    collection("Ropa");
 /** @type {HTMLFormElement} */
 const forma = document["forma"];
 getAuth().onAuthStateChanged(
@@ -38,21 +38,21 @@ async function guarda(evt) {
     evt.preventDefault();
     const formData =
       new FormData(forma);
-    const matricula = getString(
-        formData, "matricula").trim();  
-    const nombre = getString(formData, "nombre").trim();
-    const telefono = getString(formData, "telefono").trim();
-    const grupo = getString(formData, "grupo").trim();
+    const cod = getString(
+        formData, "cod").trim();  
+    const prenda = getString(formData, "prenda").trim();
+    const marca = getString(formData, "marca").trim();
+    const talla = getString(formData, "talla").trim();
     const fecha = getString(formData, "fecha").trim();
     /**
      * @type {
         import("./tipos.js").
                 Alumno} */
     const modelo = {
-      matricula,
-      nombre,
-      telefono,
-      grupo,
+      cod,
+      prenda,
+      marca,
+      talla,
       fecha 
     };
     await daoAlumno.
